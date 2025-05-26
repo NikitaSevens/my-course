d1eb94c763f6a9e7d0924998b7abccc80417d59b
NikitaSeven <148809907+NikitaSeven@users.noreply.github.com>
2025-05-26 14:30:09 +0400

    saf

--- src/Pages/Dashboard/CourseList/CourseList.tsx
+++ src/Pages/Dashboard/CourseList/CourseList.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import styles from "./CourseList.module.css";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface Course {
   id: string;
