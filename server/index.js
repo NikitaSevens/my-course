3d1dd76c8dd3d2eb9d86f1380ae6db708d156c61
NikitaSevens <mariarezanova3@gmail.com>
2025-12-25 12:16:59 +0400

    Update index.js
    замена на /.*/

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // важно для preflight (OPTIONS)
+app.options(/.*/, cors(corsOptions)); // важно для preflight (OPTIONS)
 
 app.use(express.json({ limit: "2mb" }));
 
